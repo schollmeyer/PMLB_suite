77f57638b5a8c2f75fd2d76f2a9efd9f5d037aa9
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2023-12-21 10:11:10 +0100

    gs

--- main_file.R
+++ main_file.R
@@ -1,3 +1,7 @@
+loss <- function(predictions,true_labels){mean((predictions>=0.5)==true_labels)}
+
+
+
 #Welche Methoden:
 
 
@@ -13,65 +17,80 @@
 
 install.packages("pmlbr")
 
-
+library(rpart)
 library(pmlbr)
 library(e1071)
-library(pROC)
+#library(pROC)
+install.packages("randomForest")
 library(randomForest)
 t <-0
 
-for(k in (1:78)){
+n_datasets <- length(classification_dataset_names)
 
-
-
-
-
-index <- which(summary_stats[,1]==classification_dataset_names[k])
-
-metadat <- summary_stats[index,]
-
+metadat <- list()
+for(k in (1:n_datasets)){
+  index <- which(summary_stats[,1]==classification_dataset_names[k])
+  metadat[[k]] <- summary_stats[index,]
 }
 
- metadat$n_instances %in% c(10:2000) & metadat$n_classes==2 & metadat$task== "classification")
- 
- 
-# {
- 
- 
- #if(
-print(k)
-dat <- fetch_data(classification_dataset_names[k])
-t <- t+1
+index <- NULL
+for(k in (1:n_datasets)){
+ if((metadat[[k]])$n_instances %in% c(10:20000) & (metadat[[k]])$n_classes==2 & (metadat[[k]])$task== "classification"){index <- c(index, k)}
+ }
+
+
+length(index)
+
+datasets <- list()
+t <- 1
+for(k in index){
+
+  datasets[[t]] <- fetch_data(classification_dataset_names[k])
+  t <- t+1
+  print(c("t",t))
 }
-print(c("t",t))
-
-n_test <-round(metadat$n_instances/2)
-
-i_test <- sample((1:metadat$n_instances),size=n_test)
-
-x_test <- x[i_test,]
-y_test <- y[i_test]
-
-x_train <- x[- i_test,]
-y_train <- y[- i_test]
 
 
-m <- nrow(dat)
+for(k in seq_len(length(datasets))){
 
-p <-ncol(dat)
+
+
+
+
+# n_test <-round(metadat$n_instances/2)
+#
+# i_test <- sample((1:metadat$n_instances),size=n_test)
+#
+# x_test <- x[i_test,]
+# y_test <- y[i_test]
+#
+# x_train <- x[- i_test,]
+# y_train <- y[- i_test]
+#
+#
+# m <- nrow(dat)
+#
+# p <-ncol(dat)
 #####
 #####
 #      SVM
-probs_svm <- probs_rpart <- rep(0,m)
+   dat <- datasets[[k]]
+   p <- ncol(dat)
+   m <- nrow(dat)
+   probs_svm <- probs_rpart <- rep(0,m)
 
-for(l in (1:m)){
 
-models_svm <- tune(svm, train.x=dat[-l,-p],train.y=dat$target[-l],
-ranges = list(gamma = 2^(-1:1), cost = 2^(2:4)),
-tunecontrol = tune.control(sampling = "fix"),best.model=TRUE,probability=TRUE,kernel="radial"
-)
 
-probs_svm[l] <- (predict(models_svm$best.model,dat[l,-p],probability=TRUE))#$probabilities[,2]
+   svm_model <- svm(dat[,-p],dat[,p],scale=FALSE,probability=FALSE,cross=10)
+
+   for(l in (1:m)){
+
+      models_svm <- tune(svm, train.x=dat[-l,-p],train.y=dat$target[-l],
+      ranges = list(gamma = 2^(-1:1), cost = 2^(2:4)),
+      tunecontrol = tune.control(sampling = "fix"),best.model=TRUE,probability=FALSE,kernel="radial",scale=FALSE)
+      predictions_svm[l] <- (predict(models_svm$best.model,dat[l,-p],probability=TRUE))#$probabilities[,2]
+      print(l)
+   }
 
 
 
@@ -84,13 +103,13 @@ probs_svm[l] <- (predict(models_svm$best.model,dat[l,-p],probability=TRUE))#$pro
 #      RPART
 
 
+model_rpart <- rpart(target~.,dat=dat)
+
+
+
+
+
 models_rpart <- tune.rpart(target~.,data=dat[-l,], minsplit = c(5,10,15))
-
-
- 
-
-
-
 probs_rpart[l] <- predict(models_rpart$best.model,dat[l,])
 }
 
