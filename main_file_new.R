12304c6e58972acdb6118fd0ad48a4588c9c48a1
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2023-12-22 09:57:12 +0100

    gs

--- main_file_new.R
+++ main_file_new.R
@@ -26,7 +26,7 @@ perturbate_y <- function(y,p_percent=20,set.seed=TRUE,seed=1234567){
 
 perturbate_x <- function(x,p_percent=20,set.seed=TRUE,seed=1234567){
   if(set.seed){set.seed(seed)}
-  
+
   for(k in seq_len(ncol(x))){
     indexs <- which( sample(c(1,0),size=nrow(x),prob=c(p_percent/100,(100-p_percent)/100),replace=TRUE)==1)
     length_indexs <- length(indexs)
@@ -38,7 +38,7 @@ perturbate_x <- function(x,p_percent=20,set.seed=TRUE,seed=1234567){
 
 
 
-#Welche Methoden:
+#compared methods:
 
 
 #SVM
@@ -55,9 +55,10 @@ perturbate_x <- function(x,p_percent=20,set.seed=TRUE,seed=1234567){
 
 #install.packages("pmlbr")
 library(caret)
-library(Rweka)
+library(RWeka)
 library(ranger)
 library(kknn)
+library(RSNNS)
 library(pmlbr)
 library(e1071)
 library(pROC)
@@ -69,7 +70,7 @@ datasets <- list()
 for(k in seq_len(length(classification_dataset_names))){
   index <- which(summary_stats[,1]==classification_dataset_names[k])
   metadat <- summary_stats[index,]
-  if( metadat$n_instances %in% c(10:10000) & metadat$n_classes==2 & metadat$task== "classification"){
+  if( metadat$n_instances %in% c(10:2000) & metadat$n_classes==2 & metadat$task== "classification"){
       dat <- fetch_data(classification_dataset_names[k])
       datasets[[t]] <- dat
       print(t)
@@ -79,9 +80,11 @@ for(k in seq_len(length(classification_dataset_names))){
 
 saveRDS(datasets,"datasets.RDS")
 
-
-methods <- c("svmLinear","svmRadial","J48","ranger","knn","glmnet","cre")
-for(k in seq_len(length(datasets))){
+# compared methods:
+methods <- c("svmLinear","svmRadial","J48","ranger","knn","glmnet","mlpML","cre")
+results <- array(0,c(length(datasets),3*length(methods)))
+colnames(results) <- rep(methods,3)
+for(k in (2:length(datasets))){
   dat <- datasets[[k]]
   dim(dat)
   n_row <- nrow(dat)
@@ -92,58 +95,59 @@ for(k in seq_len(length(datasets))){
   y_noisy <- perturbate_y(y)
   x_noisy <- perturbate_x(x)
   folds <- compute_k_folds(n_row,k=10)
-  
-  
-  clean_accuracies <- array(0,c(10,7))
+
+
+  clean_accuracies <- array(0,c(10,8))
   colnames(clean_accuracies) <- methods
   accuracies_noisy_y <- clean_accuracies
   accuracies_noisy_x <- clean_accuracies
-  
+
+
   for(l in (1:10)){
     indexs <- which(folds==l)
     x_train <- x[-indexs,]
-	y_train <- y[-indexs]
-	x_test <- x[indexs,]
-	y_test <- y[indexs]
-	
-	x_train_noisy <- x_noisy[-indexs,]
-	y_train_noisy <- y_noisy[-indexs]
-	x_test_noisy <- x_noisy[indexs,]
-	y_test_noisy <- y_noisy[indexs]
-	
-	
-	
+	  y_train <- y[-indexs]
+	  x_test <- x[indexs,]
+	  y_test <- y[indexs]
+
+	  x_train_noisy <- x_noisy[-indexs,]
+	  y_train_noisy <- y_noisy[-indexs]
+	  x_test_noisy <- x_noisy[indexs,]
+	  y_test_noisy <- y_noisy[indexs]
+
+
+
 	### clean data
-	
-	
-	for(i in (1:6)){
+
+
+	for(i in (1:7)){
 	   method <- methods[i]
-	   model <- train(x=x_train,y=as.factor(y_train),method = method)
+	   model <- caret::train(x=x_train,y=as.factor(y_train),method = method)
 	   accuracy <- compute_accuracy(predict(model,x_test),y_test)
 	   clean_accuracies[l,i] <- accuracy
 	   print(c(method,accuracy))
 	 }
-	 
+
 	### perturbation in y
 	for(i in (1:6)){
 	   method <- methods[i]
-	   model <- train(x=x_train,y=as.factor(y_train_noisy),method = method)
+	   model <- caret::train(x=x_train,y=as.factor(y_train_noisy),method = method)
 	   accuracy <- compute_accuracy(predict(model,x_test),y_test_noisy)
 	   accuracies_noisy_y[l,i] <- accuracy
 	   print(c(method,accuracy))
 	}
-	
+
 	### perturbation in x
 	for(i in (1:6)){
 	   method <- methods[i]
-	   model <- train(x=x_train_noisy,y=as.factor(y_train),method = method)
+	   model <- caret::train(x=x_train_noisy,y=as.factor(y_train),method = method)
 	   accuracy <- compute_accuracy(predict(model,x_test_noisy),y_test)
 	   accuracies_noisy_x[l,i] <- accuracy
 	   print(c(method,accuracy))
 	}
-
+}
+results[k,] <- c(colMeans(clean_accuracies),colMeans(accuracies_noisy_y),colMeans(accuracies_noisy_x))
 
 }
-}	
-	
+
 
